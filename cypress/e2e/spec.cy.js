2ee94443aa55bed9988e7de8b8efd9c547dd1ce9
VictorANM2 <victor.murta2@gmail.com>
2024-06-28 23:16:58 -0300

    adicionando 3 testes e2e

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,59 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Edita uma tarefa', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('Tarefa a ser editada{enter}');
+
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .dblclick();
+
+    cy.get('.todo-list li .edit')
+      .type('{selectall}TP2 de Engenharia de Software{enter}');
+
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de Engenharia de Software');
+  });
+
+  it('Marca todas as tarefas como completas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}')
+      .type('Tarefa 3{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .click({ multiple: true });
+
+    cy.get('.todo-list li')
+      .each(($el) => {
+        expect($el).to.have.class('completed');
+      });
+  });
+
+  it('Limpa todas as tarefas completas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}')
+      .type('Tarefa 3{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .click({ multiple: true });
+
+    cy.contains('Clear completed')
+      .click();
+
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+  });
 });
